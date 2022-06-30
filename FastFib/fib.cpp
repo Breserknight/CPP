e90d78f370d373eb18f4ee46c6cf8f46d7b57e63
Jerome Joseph <82434071+Breserknight@users.noreply.github.com>
2022-07-01 00:25:18 +0530

    Add files via upload

---
+++ FastFib/fib.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<string>
+#define MAX 10000000
+
+using namespace std;
+
+string matrix[MAX];
+void initialize(){
+    for(int i = 0; i < MAX; i++) matrix[i] = "/";
+}
+
+bool empty(string A){
+    if(A == "/") return true;
+    return false;
+}
+
+string stradd(string A, string B){
+    int len = A.length() - B.length();
+    if(len > 0){
+        string s="0";
+        for(int i = 1; i < len; i++) s += "0";
+        B = s+B; 
+    }
+    else if(len < 0){
+        string s="0";
+        for(int i = 1; i < abs(len); i++) s+="0";
+        A =s+A;
+    }
+    int sum = 0, carry = 0, n = A.length();
+    for(int i = n-1; i >=0; i--){
+        sum = carry + (int)A[i] + (int)B[i] - '0' - '0';
+        carry = sum / 10;
+
+        sum = sum % 10;
+        A[i] = char(sum + '0'); 
+
+    }
+    if(carry > 0) A = to_string(carry) + A; 
+    return A;
+}
+
+string fib(int n){
+    if(n < 2) return to_string(n);
+    if(!empty(matrix[n])) return matrix[n];
+    matrix[n] = stradd(fib(n-1) , fib(n-2));
+    return matrix[n];
+}
+
+int main(){
+    initialize();
+    int n;
+    cout << "Enter n : ";
+    cin >> n;
+    cout << fib(n)<<endl;
+}
